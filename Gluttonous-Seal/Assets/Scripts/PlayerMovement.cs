9ab7758592e43389326a46a593c7798022117190
kelseykato <kelseykato@Kelseys-MacBook-Pro-3.local>
2016-10-04 23:31:36 -1000

    PlayerMovement

---
+++ Gluttonous-Seal/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate () 
+	{
+		var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Quaternion rotation = Quaternion.LookRotation (transform.position - mousePosition, Vector3.forward);
+
+		transform.rotation = rotation;
+		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+		GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+		float input = Input.GetAxis ("Vertical");
+		GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed * input);
+	}
+}
