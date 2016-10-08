a8a6c7a57ff2aaa07bed04bf494b4d21a4810061
kelseykato <kelseykato@Kelseys-MacBook-Pro-3.local>
2016-10-08 12:59:33 -1000

    food manager and scipts

---
+++ Gluttonous-Seal/Assets/Scripts/FoodManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodManager : MonoBehaviour 
+{
+	public GameObject food;
+	public float spawnTime = 5f;
+	public Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () 
+	{
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Spawn() 
+	{
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+		Instantiate (food, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+}
