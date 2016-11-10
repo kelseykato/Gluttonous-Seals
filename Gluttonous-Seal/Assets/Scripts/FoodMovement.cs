ce3a562058a8d460628fbf9211cdd4e6ebb13d63
kelseykato <kelseykato@dhcp-168-105-27-245.wireless.manoa.hawaii.edu>
2016-11-09 14:41:08 -1000

    seal eating animation, fish walking animation added

--- Gluttonous-Seal/Assets/Scripts/FoodMovement.cs
+++ Gluttonous-Seal/Assets/Scripts/FoodMovement.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class FoodMovement : MonoBehaviour {
 
+	private Animator animator;
+
 	public float speed;
 	Vector3 moveDirection = new Vector3(-1, 0, 0);
-	bool movingLeft = false;
+	bool movingLeft = true;
 
 	// Use this for initialization
 	void Start () {
 
+		animator = GetComponent<Animator> ();
 
 	}
 	
@@ -17,9 +20,13 @@ public class FoodMovement : MonoBehaviour {
 	void Update () {
 
 		if (transform.localPosition.x == 7) {
+			animator.SetBool ("Left", true);
+			animator.SetBool ("Right", false);
 			moveDirection = new Vector3 (-1, 0, 0);
 		}
 		if (transform.localPosition.x == -7) {
+			animator.SetBool ("Right", true);
+			animator.SetBool ("Left", false);
 			moveDirection = new Vector3 (1, 0, 0);
 		}
 		if (!movingLeft && transform.localPosition.x >= 7) {
